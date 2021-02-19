1d78bf136054e84b1c4334181f98f108c4349f78
FFbsilva <35637163+FFbsilva@users.noreply.github.com>
2021-02-18 23:01:37 -0300

    Adicionado as linhas de rede

--- ShellScripts/writingSource.sh
+++ ShellScripts/writingSource.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+
+#Run like root users
+writeNetworkSet()
+{
+cat<<EOF> $1
+
+# This file describes the network interfaces available on your system
+# and how to activate them. For more information, see interfaces(5).
+
+source /etc/network/interfaces.d/*
+
+# The loopback network interface
+auto lo
+iface lo inet loopback
+# The custom configuration.Can be modified
+allow-hotplug enp3s0
+iface enp3s0 inet static
+    address 192.168.1.25
+    network 192.168.1.1
+    gateway 192.168.1.200
+    broadcast 192.168.255.255
+    netmask 255.255.0.0
+    dns-servers 8.8.8.8 8.8.4.4
+EOF
+}
+
 writeLines()
 {
 cat <<EOF> $1
@@ -13,7 +39,12 @@ deb-src http://deb.debian.org/debian buster-updates main contrib non-free
 EOF
 }
 
-file=/home/felipe/Documentos/ShellScripts/soul.list
+file5=/etc/apt/sources.list
+file=/etc/network/interfaces
 
-writeLines $file
+writeLines $file5
+writeNetworkSet $file
+
 #sudo apt-get update -y
+#sudo apt-get install samba -y
+#sudo apt-get install 
